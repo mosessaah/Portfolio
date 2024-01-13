72be0b8dddcfdc5e6cd10b0096bf7b1a8d47518c
Moses Saah <mosessaah@oru.edu>
2024-01-12 18:50:10 -0600

    modify

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const app = express(); // Initialize
 const port = 3000; // Port number for now on my computer 
 app.set('view engine', 'ejs');  // 
 app.use(express.urlencoded({extended: true}));      // Set our body passer
-app.use(express.static("public"));                  // public folder 
+app.use(express.static("public"));                  //  public folder 
 
 app.get("/", (req, res)=>{
     res.render("home");
+});
+
+app.get("/about", (req, res)=>{
+    res.render("about");
+});
+app.get("/portfolio", (req, res)=>{
+    res.render("portfolio");
+});
+app.get("/contact", (req, res)=>{
+    res.render("contact");
 });
 
 // Server Listening on the port number 
